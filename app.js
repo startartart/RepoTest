1e1b9d75e654425f639c8804cbcee9feecdaa2ad
ParkByunggwon <startartart@naver.com>
2024-04-29 17:15:17 +0900

    feat: newtuber API 구현 모든 뉴튜버 조회 등록된 id에 해당하는 뉴튜버 조회 뉴튜버 등록

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World');
+});
+
+const newtuber1 = {
+  channelTitle: 'newTuber1',
+  sub: 0,
+  videoNum: 0,
+};
+
+const newtuber2 = {
+  channelTitle: 'newTuber2',
+  sub: 0,
+  videoNum: 0,
+};
+
+const newtuber3 = {
+  channelTitle: 'newTuber3',
+  sub: 0,
+  videoNum: 0,
+};
+
+const db = new Map();
+let idx = 1;
+
+db.set(idx++, newtuber1);
+db.set(idx++, newtuber2);
+db.set(idx++, newtuber3);
+
+app.get('/newtuber/:id', (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+
+  const newtuber = db.get(id);
+  if (newtuber == undefined)
+    res.json({
+      message: '뉴튜버 정보를 찾을 수 없습니다.',
+    });
+  else res.json(newtuber);
+});
+
+app.get('/newtubers', (req, res) => {
+  res.json(Object.fromEntries(db));
+});
+
+app.post('/newtuber', (req, res) => {
+  db.set(idx++, req.body);
+
+  res.json({
+    message: `${db.get(idx - 1).channelTitle}님은 ${idx}번째 뉴튜버입니다.`,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`example app listening on port ${port}`);
+});

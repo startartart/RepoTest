a750f85cf80959e137406b9a0c794546f4e5dad9
ParkByunggwon <startartart@naver.com>
2024-05-01 20:52:43 +0900

    feat: join API 구현

--- project-newtube/app.js
+++ project-newtube/app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-app.use(express.json());
+
+const userDB = new Map();
+let userIdx = 1;
 
 app.post('/login', (req, res) => {});
 
-app.post('/join', (req, res) => {});
+app.use(express.json());
+app.post('/join', (req, res) => {
+  if (!req.body.userId || !req.body.pwd || !req.body.name)
+    res
+      .status(400)
+      .json({ message: '올바르지 않거나 입력되지 않은 형식입니다.' });
+  else {
+    userDB.set(userIdx++, req.body);
+    res.status(201).json({
+      message: `${userDB.get(userIdx - 1).name}님. 회원가입에 성공하셨습니다.`,
+    });
+  }
+});
 
 app.get('/user/:id', (req, res) => {});
 
 app.delete('/user/:id', (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`example app listening on port ${port}`);
+});

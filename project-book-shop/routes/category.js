59d556bc2e2fe183e2f514c245c62b6e6e203019
ParkByunggwon <startartart@naver.com>
2024-05-23 22:26:37 +0900

    feat: book, category API 구현

---
+++ project-book-shop/routes/category.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { allCategory } = require('../controller/CategoryController');
+
+router.use(express.json());
+
+router.get('/', allCategory);
+
+
+module.exports = router;

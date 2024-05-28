01d5ebe7f6383eaad0196cc9ea919af900a21770
ParkByunggwon <startartart@naver.com>
2024-05-28 10:30:43 +0900

    feat: carts API 구현 장바구니 추가, 조회, 삭제

--- project-book-shop/routes/carts.js
+++ project-book-shop/routes/carts.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const {
+  addToCart,
+  getCartItems,
+  removeCartItem,
+} = require('../controller/CartController');
 
 router.use(express.json());
 
-router.post('/', (req, res) => {
-  res.json('장바구니 담기');
-});
+router.post('/', addToCart);
 
-router.get('/', (req, res) => {
-  res.json('장바구니 조회');
-});
+router.get('/', getCartItems);
 
-router.delete('/:id', (req, res) => {
-  res.json('장바구니 도서 삭제');
-});
+router.delete('/:id', removeCartItem);
 
 module.exports = router;

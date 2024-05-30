6b2f728a7017788e6a7671f130948336ab37071c
ParkByunggwon <startartart@naver.com>
2024-05-30 21:11:03 +0900

    style: 주석 변경

--- project-book-shop/controller/OrderController.js
+++ project-book-shop/controller/OrderController.js
@@ -16,40 +16,40 @@ const order = (req, res) => {
   } = req.body;
   const sql = `insert into delivery (address, receiver, contact) values (?, ?, ?)`;
   const values = [delivery.address, delivery.receiver, delivery.contact];
-  const delivery_id = 1;
-  const order_id = 1;
+  const delivery_id = 3;
+  const order_id = 2;
 
-  //   conn.query(sql, values, (err, results) => {
-  //     if (err) return res.status(StatusCodes.BAD_REQUEST).end();
+    // conn.query(sql, values, (err, results) => {
+    //   if (err) return res.status(StatusCodes.BAD_REQUEST).end();
 
-  //     const delivery_id = results.insertId;
+    //   const delivery_id = results.insertId;
 
-  //     if (results) return res.status(StatusCodes.CREATED).json(results);
-  //     return res.status(StatusCodes.NOT_FOUND).end();
-  //   });
+    //   if (results) return res.status(StatusCodes.CREATED).json(results);
+    //   return res.status(StatusCodes.NOT_FOUND).end();
+    // });
 
-  const sql2 = `insert into orders (book_title, total_quantity, total_price, user_id, delivery_id)
-                values (?, ?, ?, ?, ?)`;
-  const values2 = [
-    first_book_title,
-    total_quantity,
-    total_price,
-    user_id,
-    delivery_id,
-  ];
+//   const sql2 = `insert into orders (book_title, total_quantity, total_price, user_id, delivery_id)
+//                 values (?, ?, ?, ?, ?)`;
+//   const values2 = [
+//     first_book_title,
+//     total_quantity,
+//     total_price,
+//     user_id,
+//     delivery_id,
+//   ];
 
-  //   conn.query(sql2, values2, (err, results) => {
-  //     if (err) return res.status(StatusCodes.BAD_REQUEST).end();
+//     conn.query(sql2, values2, (err, results) => {
+//       if (err) return res.status(StatusCodes.BAD_REQUEST).end();
 
-  //     const order_id = results.insertId;
+//       const order_id = results.insertId;
 
-  //     if (results) return res.status(StatusCodes.CREATED).json(results);
-  //     return res.status(StatusCodes.NOT_FOUND).end();
-  //   });
+//       if (results) return res.status(StatusCodes.CREATED).json(results);
+//       return res.status(StatusCodes.NOT_FOUND).end();
+//     });
 
   const sql3 = `insert into orderedBook (order_id, book_id, quantity) values ?`;
   let values3 = [];
-  
+
   items.forEach((item) => {
     values3.push([order_id, item.book_id, item.quantity]);
   });

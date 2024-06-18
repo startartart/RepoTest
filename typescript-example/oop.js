bb737231b841bb4346ad560e187fd8a6d5611bc0
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2024-06-18 23:18:57 +0900

    javascript 컴파일 추가

---
+++ typescript-example/oop.js
@@ -0,0 +1,30 @@
+var Employee = /** @class */ (function () {
+    function Employee(_empName, _age, _empJob) {
+        var _this = this;
+        this._empName = _empName;
+        this._age = _age;
+        this._empJob = _empJob;
+        this.printEmp = function () {
+            console.log(_this._empName +
+                '의 나이는' +
+                _this._age +
+                '이고, 직업은' +
+                _this._empJob +
+                '입니다.');
+        };
+    }
+    Object.defineProperty(Employee.prototype, "empName", {
+        get: function () {
+            return this._empName;
+        },
+        set: function (val) {
+            this._empName = val;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Employee;
+}());
+var employee1 = new Employee('park', 26, '개발자');
+employee1.empName = 'gwo';
+employee1.printEmp();

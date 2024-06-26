f2c61ac44f2ef24fd795cbf884521222f52a98eb
ParkByunggwon <startartart@naver.com>
2024-06-26 22:55:33 +0900

    taskApp 컴포넌트 구현 메인 삭제 및 로그 버튼, 로그모달, 로그 아이템, 모달생성바

--- typescript-example/task-exam/src/components/LoggerModal/LogItem/LogItem.tsx
+++ typescript-example/task-exam/src/components/LoggerModal/LogItem/LogItem.tsx
@@ -1,3 +1,27 @@
-export default function LogItem() {
-  return <>LogItem</>;
+import { BsFillPersonFill } from 'react-icons/bs';
+import { ILogItem } from '../../../types';
+import { logItemWrap, message, author, date } from './LogItem.css';
+
+type TLogItemProps = {
+  logItem: ILogItem;
+};
+
+export default function LogItem({ logItem }: TLogItemProps) {
+  const timeOffset = new Date(Date.now() - Number(logItem.logTimestamp));
+
+  const showOffsetTime = `
+  ${timeOffset.getMinutes() > 0 ? `${timeOffset.getMinutes()}m` : ``}
+  ${timeOffset.getSeconds() > 0 ? `${timeOffset.getSeconds()}s ago` : ``}
+  ${timeOffset.getSeconds() === 0 ? `just now` : ``}
+  `;
+  return (
+    <div className={logItemWrap}>
+      <div className={author}>
+        <BsFillPersonFill />
+        {logItem.logAuthor}
+      </div>
+      <div className={message}>{logItem.logMessage}</div>
+      <div className={date}>{showOffsetTime}</div>
+    </div>
+  );
 }
--- typescript-example/task-exam/src/components/LoggerModal/LoggerModal.tsx
+++ typescript-example/task-exam/src/components/LoggerModal/LoggerModal.tsx
@@ -1,3 +1,35 @@
-export default function LoggerModal() {
-  return <>LoggerModal</>;
+import { Dispatch, SetStateAction } from 'react';
+import { useTypedSelector } from '../../hooks/redux';
+import { FiX } from 'react-icons/fi';
+import LogItem from './LogItem/LogItem';
+import {
+  wrapper,
+  modalWindow,
+  header,
+  title,
+  closeButton,
+  body,
+} from './LoggerModal.css';
+
+type TLoggerModalProps = {
+  setIsLoggerOpen: Dispatch<SetStateAction<boolean>>;
+};
+export default function LoggerModal({ setIsLoggerOpen }: TLoggerModalProps) {
+  const logs = useTypedSelector((state) => state.logger.logArray);
+
+  return (
+    <div className={wrapper}>
+      <div className={modalWindow}>
+        <div className={header}>
+          <div className={title}>활동 기록</div>
+          <FiX className={closeButton} onClick={() => setIsLoggerOpen(false)} />
+        </div>
+        <div className={body}>
+          {logs.map((log) => (
+            <LogItem key={log.logId} logItem={log} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
